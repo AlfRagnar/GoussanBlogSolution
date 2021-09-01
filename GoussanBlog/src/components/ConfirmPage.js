389cc32791d8d7eb52dcbff40c8f06abddd139f8
Alf Ragnar <69853212+AlfRagnar@users.noreply.github.com>
2021-09-01 11:47:49 +0200

    Update ConfirmPage.js

--- GoussanBlog/src/components/ConfirmPage.js
+++ GoussanBlog/src/components/ConfirmPage.js
@@ -3,5 +3,13 @@ import React from "react";
 export default function ConfirmPage(props) {
   var token = props.match.params.token;
 
-  return <div>Your token is: {token}</div>;
+  return (
+    <>
+      <div>Your token is: {token}</div>
+      <div>
+        This is where the Confirmation Logic will be ran. Will most likely just
+        be an API POST request with the token
+      </div>
+    </>
+  );
 }

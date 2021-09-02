c2b5043f7a5aabe9281561000c10514488d44a83
Alf Ragnar <69853212+AlfRagnar@users.noreply.github.com>
2021-09-02 12:36:00 +0200

    Update Home.js
    Added disclaimer for home page

--- GoussanBlog/src/containers/Home.js
+++ GoussanBlog/src/containers/Home.js
@@ -65,7 +65,11 @@ export default function Home() {
                 Goussanjarga
               </Typography>
               <Typography style={{ color: theme.text }} variant="body2">
-                A Simple Media Sharing Website
+                A Simple Media Sharing Website Website is currently under
+                development so any content made or uploaded to this website are
+                up for change and any integrity or data protection is not
+                guaranteed. Chances are the website database and file storage
+                will be wiped multiple times a day.
               </Typography>
               {auth ? (
                 <ViewJWTButton />

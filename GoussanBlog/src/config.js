0d559c9379e9a270cc720750585578f404825242
Alf Ragnar <69853212+AlfRagnar@users.noreply.github.com>
2021-09-03 09:23:22 +0200

    Update config.js

--- GoussanBlog/src/config.js
+++ GoussanBlog/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  defaultEndpoint: "https://localhost:5001", // Set your default endpoint for axios to make API calls to
+  defaultEndpoint: "https://goussanapi.azure-api.net", // Set your default endpoint for axios to make API calls to
 };
 
 export default config;
